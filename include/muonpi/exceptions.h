450553b0986baf6547f5efc64aa9eccf235113d1
Daniel Treffenstädt <daniel@treffenstaedt.de>
2021-07-23 09:14:48 +0200

    Fixed resizing and initialisation of upper_matrix structure

--- include/muonpi/exceptions.h
+++ include/muonpi/exceptions.h
@@ -23,9 +23,9 @@ void inline terminate_handler()
 }
 
 namespace boost {
-void LIBMUONPI_PUBLIC assertion_failed_msg(char const* expr, char const* msg, char const* function, char const* /*file*/, long /*line*/);
+void assertion_failed_msg(char const* expr, char const* msg, char const* function, char const* /*file*/, long /*line*/);
 
-void LIBMUONPI_PUBLIC assertion_failed(char const* expr, char const* function, char const* file, long line);
+void assertion_failed(char const* expr, char const* function, char const* file, long line);
 } // namespace boost
 
 #endif // EXCEPTIONS_H
