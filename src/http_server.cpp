c6f4b6d7b5192043dffcc028f8b724a2c933e03e
Daniel Treffenstädt <daniel@treffenstaedt.de>
2021-10-01 22:22:04 +0200

    Added support for boost version 1.67 for rpi
    raspberrypi OS only provides boost version 1.67, where the boost::beast
    streams were not available yet.

--- src/http_server.cpp
+++ src/http_server.cpp
@@ -3,183 +3,13 @@
 #include "muonpi/log.h"
 #include "muonpi/scopeguard.h"
 
+#include "detail/http_session.hpp"
+
 #include <sstream>
 #include <utility>
 
+
 namespace muonpi::http {
-
-template <typename Stream>
-class session {
-public:
-    explicit session(tcp::socket&& socket, ssl::context& ctx, std::function<response_type(request_type)> handler);
-    explicit session(tcp::socket&& socket, std::function<response_type(request_type)> handler);
-
-    void run();
-
-    void do_read();
-
-    void do_close();
-
-    void on_read(beast::error_code errorcode, std::size_t bytes_transferred);
-
-    void on_write(bool close, beast::error_code ec, std::size_t bytes_transferred);
-
-private:
-    void notify();
-
-    Stream m_stream;
-
-    beast::flat_buffer m_buffer;
-    request_type m_req;
-    std::shared_ptr<void> m_res;
-
-    std::function<response_type(request_type)> m_handler;
-
-    std::condition_variable m_done {};
-    std::mutex m_mutex {};
-
-    constexpr static std::chrono::duration s_timeout { std::chrono::seconds { 30 } };
-};
-
-template <>
-session<beast::ssl_stream<beast::tcp_stream>>::session(tcp::socket&& socket, ssl::context& ctx, std::function<response_type(request_type)> handler)
-    : m_stream { std::move(socket), ctx }
-    , m_handler { std::move(handler) }
-{
-}
-
-template <>
-session<beast::tcp_stream>::session(tcp::socket&& socket, std::function<response_type(request_type)> handler)
-    : m_stream { std::move(socket) }
-    , m_handler { std::move(handler) }
-{
-}
-
-template <>
-void session<beast::ssl_stream<beast::tcp_stream>>::run()
-{
-    beast::get_lowest_layer(m_stream).expires_after(s_timeout);
-
-    m_stream.async_handshake(ssl::stream_base::server, [&](beast::error_code ec) {
-        scope_guard guard { [&] { notify(); } };
-        if (ec) {
-            fail(ec, "handshake");
-            return;
-        }
-        guard.dismiss();
-        do_read();
-    });
-
-    std::unique_lock<std::mutex> lock { m_mutex };
-    m_done.wait(lock);
-}
-
-template <>
-void session<beast::tcp_stream>::run()
-{
-    beast::get_lowest_layer(m_stream).expires_after(s_timeout);
-
-    net::dispatch(m_stream.get_executor(), [&] { do_read(); });
-
-    std::unique_lock<std::mutex> lock { m_mutex };
-    m_done.wait(lock);
-}
-
-template <>
-void session<beast::ssl_stream<beast::tcp_stream>>::do_close()
-{
-    scope_guard guard { [&] { notify(); } };
-    beast::get_lowest_layer(m_stream).expires_after(s_timeout);
-    m_stream.async_shutdown([&](beast::error_code ec) {
-        if (ec) {
-            fail(ec, "shutdown");
-        }
-        notify();
-    });
-    guard.dismiss();
-}
-
-template <>
-void session<beast::tcp_stream>::do_close()
-{
-    scope_guard guard { [&] { notify(); } };
-    beast::get_lowest_layer(m_stream).expires_after(s_timeout);
-    beast::error_code ec;
-    m_stream.socket().shutdown(tcp::socket::shutdown_send, ec);
-    if (ec) {
-        fail(ec, "shutdown");
-    }
-    guard.dismiss();
-}
-
-template <typename Stream>
-void session<Stream>::do_read()
-{
-    scope_guard guard { [&] { notify(); } };
-    m_req = {};
-
-    beast::get_lowest_layer(m_stream).expires_after(s_timeout);
-
-    beast::http::async_read(m_stream, m_buffer, m_req, [&](beast::error_code ec, std::size_t bytes_transferred) { on_read(ec, bytes_transferred); });
-    guard.dismiss();
-}
-
-template <typename Stream>
-void session<Stream>::on_read(beast::error_code errorcode, std::size_t bytes_transferred)
-{
-    scope_guard guard { [&] { notify(); } };
-    boost::ignore_unused(bytes_transferred);
-
-    if (errorcode == beast::http::error::end_of_stream) {
-        do_close();
-        return;
-    }
-
-    if (errorcode) {
-        fail(errorcode, "read");
-        return;
-    }
-
-    auto sp { std::make_shared<beast::http::message<false, beast::http::string_body>>(m_handler(std::move(m_req))) };
-
-    bool close { sp->need_eof() };
-    m_res = sp;
-    beast::http::async_write(
-        m_stream,
-        *sp,
-        [&](beast::error_code ec, std::size_t bytes) {
-            on_write(close, ec, bytes);
-        });
-    guard.dismiss();
-}
-
-template <typename Stream>
-void session<Stream>::on_write(bool close, beast::error_code ec, std::size_t bytes_transferred)
-{
-    scope_guard guard { [&] { notify(); } };
-    boost::ignore_unused(bytes_transferred);
-
-    if (ec) {
-        fail(ec, "write");
-        return;
-    }
-
-    if (close) {
-        do_close();
-        return;
-    }
-
-    m_res = nullptr;
-
-    do_read();
-    guard.dismiss();
-}
-
-template <typename Stream>
-void session<Stream>::notify()
-{
-    m_done.notify_all();
-}
 
 http_server::http_server(configuration config)
     : thread_runner("http", true)
@@ -244,10 +74,10 @@ void http_server::do_accept()
         } else {
             std::thread([&] {
                 if (m_conf.ssl) {
-                    session<beast::ssl_stream<beast::tcp_stream>> sess { std::move(socket), m_ctx, [&](request_type req) { return handle(std::move(req)); } };
+                    detail::session<detail::ssl_stream_t> sess { std::move(socket), m_ctx, [&](request_type req) { return handle(std::move(req)); } };
                     sess.run();
                 } else {
-                    session<beast::tcp_stream> sess { std::move(socket), [&](request_type req) { return handle(std::move(req)); } };
+                    detail::session<detail::tcp_stream_t> sess { std::move(socket), [&](request_type req) { return handle(std::move(req)); } };
                     sess.run();
                 }
             }).detach();
