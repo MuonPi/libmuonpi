a3ec6988fc081e67df3a98c8432c833748707968
Daniel Treffenstädt <daniel@treffenstaedt.de>
2022-01-29 14:07:41 +0100

    Added tests for data_series sample code

--- tests/core/dataseries.cpp
+++ tests/core/dataseries.cpp
@@ -8,13 +8,15 @@ BOOST_AUTO_TEST_SUITE(core_data_series_test, * utf::tolerance(0.00001))
 
 BOOST_AUTO_TEST_CASE(empty_test)
 {
-    muonpi::data_series<double> series{10};
+    using data_s = muonpi::data_series<double, false>;
+
+    data_s series{10};
 
     BOOST_TEST(series.n() == 0);
 
-    BOOST_TEST(series.mean(muonpi::data_series<double>::mean_t::arithmetic) == 0.0);
-    BOOST_TEST(series.mean(muonpi::data_series<double>::mean_t::geometric) == 0.0);
-    BOOST_TEST(series.mean(muonpi::data_series<double>::mean_t::harmonic) == 0.0);
+    BOOST_TEST(series.mean(data_s::mean_t::arithmetic) == 0.0);
+    BOOST_TEST(series.mean(data_s::mean_t::geometric) == 0.0);
+    BOOST_TEST(series.mean(data_s::mean_t::harmonic) == 0.0);
     BOOST_TEST(series.median() == 0.0);
     BOOST_TEST(series.stddev() == 0.0);
     BOOST_TEST(series.variance() == 0.0);
@@ -27,15 +29,17 @@ BOOST_AUTO_TEST_CASE(empty_test)
 
 BOOST_AUTO_TEST_CASE(fill_test)
 {
-    muonpi::data_series<double> series{10};
+    using data_s = muonpi::data_series<double, false>;
+
+    data_s series{10};
 
     BOOST_TEST(series.n() == 0);
     series.add(1.0);
     BOOST_TEST(series.n() == 1);
 
-    BOOST_TEST(series.mean(muonpi::data_series<double>::mean_t::arithmetic) == 1.0);
-    BOOST_TEST(series.mean(muonpi::data_series<double>::mean_t::geometric) == 1.0);
-    BOOST_TEST(series.mean(muonpi::data_series<double>::mean_t::harmonic) == 1.0);
+    BOOST_TEST(series.mean(data_s::mean_t::arithmetic) == 1.0);
+    BOOST_TEST(series.mean(data_s::mean_t::geometric) == 1.0);
+    BOOST_TEST(series.mean(data_s::mean_t::harmonic) == 1.0);
     BOOST_TEST(series.median() == 1.0);
     BOOST_TEST(series.stddev() == 0.0);
     BOOST_TEST(series.variance() == 0.0);
@@ -56,12 +60,85 @@ BOOST_AUTO_TEST_CASE(fill_test)
     series.add(1.5);
     series.add(0.5);
 
-    BOOST_TEST(series.mean(muonpi::data_series<double>::mean_t::arithmetic) == 1.0);
-    BOOST_TEST(series.mean(muonpi::data_series<double>::mean_t::geometric) == 0.955273);
-    BOOST_TEST(series.mean(muonpi::data_series<double>::mean_t::harmonic) == 0.833333);
+    BOOST_TEST(series.mean(data_s::mean_t::arithmetic) == 1.0);
+    BOOST_TEST(series.mean(data_s::mean_t::geometric) == 0.955273);
+    BOOST_TEST(series.mean(data_s::mean_t::harmonic) == 0.833333);
     BOOST_TEST(series.median() == 1.0);
     BOOST_TEST(series.stddev() == 0.387298);
     BOOST_TEST(series.variance() == 0.15);
+    BOOST_TEST(series.rms() == 1.07238);
+    BOOST_TEST(series.current() == 0.5);
+    BOOST_TEST(series.min() == 0.5);
+    BOOST_TEST(series.max() == 1.5);
+    BOOST_TEST(series.sum() == 10.0);
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE(core_data_series_test_sample, * utf::tolerance(0.00001))
+
+BOOST_AUTO_TEST_CASE(empty_test)
+{
+    using data_s = muonpi::data_series<double, true>;
+
+    data_s series{10};
+
+    BOOST_TEST(series.n() == 0);
+
+    BOOST_TEST(series.mean(data_s::mean_t::arithmetic) == 0.0);
+    BOOST_TEST(series.mean(data_s::mean_t::geometric) == 0.0);
+    BOOST_TEST(series.mean(data_s::mean_t::harmonic) == 0.0);
+    BOOST_TEST(series.median() == 0.0);
+    BOOST_TEST(series.stddev() == 0.0);
+    BOOST_TEST(series.variance() == 0.0);
+    BOOST_TEST(series.rms() == 0.0);
+    BOOST_TEST(series.current() == 0.0);
+    BOOST_TEST(series.min() == 0.0);
+    BOOST_TEST(series.max() == 0.0);
+    BOOST_TEST(series.sum() == 0.0);
+}
+
+BOOST_AUTO_TEST_CASE(fill_test)
+{
+    using data_s = muonpi::data_series<double, true>;
+
+    data_s series{10};
+
+    BOOST_TEST(series.n() == 0);
+    series.add(1.0);
+    BOOST_TEST(series.n() == 1);
+
+    BOOST_TEST(series.mean(data_s::mean_t::arithmetic) == 1.0);
+    BOOST_TEST(series.mean(data_s::mean_t::geometric) == 1.0);
+    BOOST_TEST(series.mean(data_s::mean_t::harmonic) == 1.0);
+    BOOST_TEST(series.median() == 1.0);
+    BOOST_TEST(series.stddev() == 0.0);
+    BOOST_TEST(series.variance() == 0.0);
+    BOOST_TEST(series.rms() == 1.0);
+    BOOST_TEST(series.current() == 1.0);
+    BOOST_TEST(series.min() == 1.0);
+    BOOST_TEST(series.max() == 1.0);
+    BOOST_TEST(series.sum() == 1.0);
+
+    series.add(1.0);
+    series.add(1.0);
+    series.add(1.0);
+
+    series.add(1.5);
+    series.add(0.5);
+    series.add(1.5);
+    series.add(0.5);
+    series.add(1.5);
+    series.add(0.5);
+
+    BOOST_TEST(series.mean(data_s::mean_t::arithmetic) == 1.0);
+    BOOST_TEST(series.mean(data_s::mean_t::geometric) == 0.955273);
+    BOOST_TEST(series.mean(data_s::mean_t::harmonic) == 0.833333);
+    BOOST_TEST(series.median() == 1.0);
+    BOOST_TEST(series.stddev() == 0.40824829);
+    BOOST_TEST(series.variance() == 0.16666666);
     BOOST_TEST(series.rms() == 1.07238);
     BOOST_TEST(series.current() == 0.5);
     BOOST_TEST(series.min() == 0.5);
