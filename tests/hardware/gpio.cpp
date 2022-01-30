440cfae528f3aaa47573ea38931ad0736979dc0f
hangeza <zaunick@exp2.physik.uni-giessen.de>
2022-01-30 15:55:40 +0100

    added another hardware test and made the instance of gpiod_handler in text fixture a unique_ptr

--- tests/hardware/gpio.cpp
+++ tests/hardware/gpio.cpp
@@ -5,17 +5,18 @@
 
 
 struct fixture_gpio_handler {
-    inline static muonpi::gpio_handler gpio {"/dev/gpiochip0", "libmuonpi-test"};
+    inline static std::unique_ptr<muonpi::gpio_handler> gpio {};
 
     void setup()
     {
         muonpi::log::system::setup(muonpi::log::Level::Info, [](int c){exit(c);}, std::cerr);
+        gpio = std::move( std::make_unique<muonpi::gpio_handler>("/dev/gpiochip0", "libmuonpi-test") );
     }
 
     void teardown()
     {
-        gpio.stop();
-        gpio.join();
+        gpio->stop();
+        gpio->join();
     }
 };
 
@@ -26,9 +27,16 @@ BOOST_AUTO_TEST_SUITE( test_gpio_access )
 
 BOOST_AUTO_TEST_CASE( test_gpio_access_init )
 {
-    auto chip = fixture_gpio_handler::gpio.get_chip_info();
+    auto chip = fixture_gpio_handler::gpio->get_chip_info();
     BOOST_TEST( (!chip.name.empty()) );
     BOOST_TEST( (chip.num_lines > 0) );
+}
+
+BOOST_AUTO_TEST_CASE( test_gpio_access_read )
+{
+    std::function<muonpi::gpio::state_t()> gpio_read_fn { fixture_gpio_handler::gpio->get_pin_input( 23, muonpi::gpio::bias_t::Disabled ) };
+    muonpi::gpio::state_t state { gpio_read_fn() };
+    BOOST_TEST( (state != muonpi::gpio::state_t::Undefined) );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
