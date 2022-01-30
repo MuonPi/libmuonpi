99257d2309e4eb6ad13a19d9d0d54c95a8165134
Daniel Treffenstädt <daniel@treffenstaedt.de>
2022-01-30 10:15:55 +0100

    Added dummy test to link tests

--- tests/link/main.cpp
+++ tests/link/main.cpp
@@ -1,2 +1,8 @@
 #define BOOST_TEST_MODULE muonpi link tests
 #include <boost/test/included/unit_test.hpp>
+namespace utf = boost::unit_test;
+
+BOOST_AUTO_TEST_CASE(http_dummy_test, * utf::expected_failures(1))
+{
+    BOOST_TEST(false); // dummy test so it isn't empty until real tests are written
+}
