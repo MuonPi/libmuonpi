5d8736fdc9a35b4ee16fc178d3648d7f7b451334
Daniel Treffenstädt <daniel@treffenstaedt.de>
2022-01-30 10:02:48 +0100

    Moved hardware dependent tests to their own unit

---
+++ tests/hardware/gpio.cpp
@@ -0,0 +1,21 @@
+#include <boost/test/unit_test.hpp>
+
+#include "muonpi/gpio_handler.h"
+
+
+
+struct fixture_gpio_handler {
+    inline static muonpi::gpio_handler gpio {"/dev/gpiochip0", "libmuonpi-test"};
+};
+
+BOOST_FIXTURE_TEST_SUITE( test_gpio_access )
+
+
+BOOST_AUTO_TEST_CASE( test_gpio_access_init )
+{
+	auto chip = fixture_gpio_handler::gpio.get_chip_info();
+	BOOST_TEST( (!chip.name.empty()) );
+	BOOST_TEST( (chip.num_lines > 0) );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
