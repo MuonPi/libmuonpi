d6ad2efd7048e4b3e0016fbee9d1b07e6c342c54
Daniel Treffenstädt <daniel@treffenstaedt.de>
2022-01-30 10:09:35 +0100

    Added dummy test to http test cases

--- tests/http/main.cpp
+++ tests/http/main.cpp
@@ -1,2 +1,9 @@
 #define BOOST_TEST_MODULE muonpi http tests
 #include <boost/test/included/unit_test.hpp>
+
+namespace utf = boost::unit_test;
+
+BOOST_AUTO_TEST_CASE(http_dummy_test, * utf::expected_failures(1))
+{
+    BOOST_TEST(false); // dummy test so it isn't empty until real tests are written
+}
