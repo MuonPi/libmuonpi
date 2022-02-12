d4286168cc6ea6ce9da6d59bed3ef152c3dee07a
hangeza <zaunick@exp2.physik.uni-giessen.de>
2022-02-12 22:55:26 +0100

    adapted i2c-example: added readout of eeprom device at address 0x50, in case one is found at this address

--- examples/i2c/src/main.cpp
+++ examples/i2c/src/main.cpp
@@ -3,8 +3,9 @@
 #include <muonpi/serial/i2cdevices.h>
 
 #include <cstdint>
-#include <iomanip>
 #include <iostream>
+#include <iomanip>
+#include <sstream>
 
 using namespace muonpi;
 
@@ -84,11 +85,41 @@ auto main() -> int
         log::error()<<"error identifying PCA9536 at 0x" << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>( i2c_extender_addr );
     }
     
-    log::info()<<"nr of instatiated devices: "<<bus.count_devices();
+    constexpr std::uint8_t eeprom_addr { 0x50 };
+    if ( bus.identify_device<serial::devices::eeproms::MC24AA02UID>( eeprom_addr ) ) {
+        // found the specific device at the expected position, so close the previously created generic
+        // i2c_device and reopen as eeprom device
+        bus.close( eeprom_addr );
+        auto& eep = bus.open<serial::devices::eeproms::MC24AA02UID>( eeprom_addr );
+        [[maybe_unused]] bool id_ok = eep.identify();
+        log::info()<<"identified "<<eep.name()<<" at 0x"
+        << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>( eeprom_addr );
+        log::info()<<"EEPROM content:";
+        std::size_t page { 0 };
+        while ( page < eep.size()/eep.page_size() ) {
+            std::ostringstream ostr;
+            ostr << std::hex << std::setw((eep.address_mode()==1)?2:4) << std::setfill('0') << page*eep.page_size() << ':';
+            std::array<std::uint8_t,eep.page_size()> page_buffer { };
+            if ( eep.read( static_cast<std::uint16_t>(page*eep.page_size()), page_buffer.data(), eep.page_size() ) != eep.page_size() ) {
+                log::error() <<"reading eeprom content";
+            }
+            for ( std::size_t page_addr { 0 };page_addr<eep.page_size();page_addr++ ) {
+                ostr << ' ' << std::hex << std::setw(2) << std::setfill('0') << static_cast<std::uint16_t>(page_buffer.at(page_addr));
+            }
+            log::info() << ostr.str();
+            page++;
+        }
+        
+    } else {
+        log::error()<<"error identifying EEPROM at 0x" << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>( eeprom_addr );
+    }
+
+    
+    log::info()<<"nr of instantiated devices: "<<bus.count_devices();
 
     // close all devices previously found
     while ( bus.count_devices() != 0U ) {
-        auto addr = bus.get_devices().begin()->second->address();
+        auto addr = bus.get_devices().begin()->first;
         bus.close( addr );
     }
     log::debug()<<"nr of rx bytes: "<<bus.rx_bytes();
