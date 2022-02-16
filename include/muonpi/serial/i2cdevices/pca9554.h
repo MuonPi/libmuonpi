066ac6d2ee6c39499c95a5730b248b5f619fa485
Daniel Treffenstädt <daniel@treffenstaedt.de>
2022-02-16 21:04:16 +0100

    Added clang-format off and on commands

--- include/muonpi/serial/i2cdevices/pca9554.h
+++ include/muonpi/serial/i2cdevices/pca9554.h
@@ -11,10 +11,12 @@ public:
     PCA9554(i2c_bus& bus, std::uint8_t address)
         : io_extender<8>(bus, address) {
         set_name("PCA9554");
+        // clang-format off
         m_addresses_hint = {
             0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27,
             0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f
         };
+        // clang-format on
     }
 };
 
