bf242d3c745be1f589db2a332b4e2d15ce19e6b1
Daniel Treffenstädt <daniel@treffenstaedt.de>
2022-04-16 12:52:45 +0200

    Applied clang-format

--- include/muonpi/serial/i2cdevices/lm75.h
+++ include/muonpi/serial/i2cdevices/lm75.h
@@ -1,8 +1,8 @@
 #ifndef MUONPI_SERIAL_I2CDEVICES_LM75_H
 #define MUONPI_SERIAL_I2CDEVICES_LM75_H
 
-#include "muonpi/serial/i2cdevice.h"
 #include "muonpi/addressrange.h"
+#include "muonpi/serial/i2cdevice.h"
 
 namespace muonpi::serial::devices {
 
@@ -30,7 +30,6 @@ public:
      */
     lm75(traffic_t& bus_traffic, const std::string& path);
 
-
     /**
      * @brief identify Attempts to positively identify the device.
      * @return True if identification was successful.
@@ -40,10 +39,11 @@ public:
     template <std::uint8_t ADDR>
     struct basic_temperature_r : public simple_register<std::uint16_t, std::uint8_t, ADDR> {
         using value_type = typename simple_register<std::uint16_t, std::uint8_t, ADDR>::value_type;
-        using address_type = typename simple_register<std::uint16_t, std::uint8_t, ADDR>::address_type;
+        using address_type =
+            typename simple_register<std::uint16_t, std::uint8_t, ADDR>::address_type;
 
-        value_type temperature : 9 {};
-        const value_type reserved : 7 {};
+        value_type       temperature : 9 {};
+        const value_type reserved    : 7 {};
 
         [[nodiscard]] constexpr auto value() const noexcept -> double {
             return static_cast<double>(temperature) / 2.0;
@@ -54,64 +54,63 @@ public:
         }
 
         constexpr explicit basic_temperature_r(value_type v) noexcept
-            : temperature { static_cast<value_type>(v >> 7) }
-            , reserved {static_cast<value_type>(v & 0b1111111) } {}
+            : temperature {static_cast<value_type>(v >> 7)}
+            , reserved {static_cast<value_type>(v & 0b1111111)} {}
 
-         constexpr explicit basic_temperature_r() noexcept = default;
+        constexpr explicit basic_temperature_r() noexcept = default;
     };
 
     struct temperature_r : public basic_temperature_r<0x00> {
-        constexpr static tag_type register_tag { i2c_register_tag::read };
+        constexpr static tag_type register_tag {i2c_register_tag::read};
 
         constexpr explicit temperature_r(value_type v) noexcept
-            : basic_temperature_r { v } {}
+            : basic_temperature_r {v} {}
 
-         constexpr explicit temperature_r() noexcept = default;
+        constexpr explicit temperature_r() noexcept = default;
     };
 
     struct t_hyst_r : public basic_temperature_r<0x01> {
-        constexpr static tag_type register_tag { i2c_register_tag::read_write };
+        constexpr static tag_type register_tag {i2c_register_tag::read_write};
 
         constexpr explicit t_hyst_r(value_type v) noexcept
-            : basic_temperature_r { v } {}
+            : basic_temperature_r {v} {}
 
-         constexpr explicit t_hyst_r() noexcept
-            : basic_temperature_r { 0b0100101100000000 } {}
+        constexpr explicit t_hyst_r() noexcept
+            : basic_temperature_r {0b0100101100000000} {}
     };
 
     struct t_os_r : public basic_temperature_r<0x02> {
-        constexpr static tag_type register_tag { i2c_register_tag::read_write };
+        constexpr static tag_type register_tag {i2c_register_tag::read_write};
 
         constexpr explicit t_os_r(value_type v) noexcept
-            : basic_temperature_r { v } {}
+            : basic_temperature_r {v} {}
 
-         constexpr explicit t_os_r() noexcept
-            : basic_temperature_r { 0b0101000000000000 } {}
+        constexpr explicit t_os_r() noexcept
+            : basic_temperature_r {0b0101000000000000} {}
     };
 
     struct configuration_r : public simple_register<std::uint8_t, std::uint8_t, 0x00> {
-        constexpr static tag_type register_tag { i2c_register_tag::read_write };
+        constexpr static tag_type register_tag {i2c_register_tag::read_write};
 
-        const value_type reserved : 3 {};
-        value_type fault_queue : 2 {};
-        value_type os_polarity : 1 {};
-        value_type comparator : 1 {};
-        value_type shutdown : 1 {};
+        const value_type reserved    : 3 {};
+        value_type       fault_queue : 2 {};
+        value_type       os_polarity : 1 {};
+        value_type       comparator  : 1 {};
+        value_type       shutdown    : 1 {};
 
         [[nodiscard]] constexpr auto get() const noexcept -> value_type override {
-            return static_cast<value_type>((fault_queue << 3) | (os_polarity << 2) | (comparator << 1) | shutdown);
+            return static_cast<value_type>((fault_queue << 3) | (os_polarity << 2)
+                                           | (comparator << 1) | shutdown);
         }
 
         constexpr explicit configuration_r(value_type v) noexcept
-            : reserved { static_cast<value_type>((v & 0b11100000) >> 5) }
-            , fault_queue { static_cast<value_type>((v & 0b11000) >> 3) }
-            , os_polarity { static_cast<value_type>((v & 0b100) >> 2) }
-            , comparator { static_cast<value_type>((v & 0b10) >> 1) }
-            , shutdown { static_cast<value_type>(v & 0b1 ) }
-        {}
+            : reserved {static_cast<value_type>((v & 0b11100000) >> 5)}
+            , fault_queue {static_cast<value_type>((v & 0b11000) >> 3)}
+            , os_polarity {static_cast<value_type>((v & 0b100) >> 2)}
+            , comparator {static_cast<value_type>((v & 0b10) >> 1)}
+            , shutdown {static_cast<value_type>(v & 0b1)} {}
 
-         constexpr explicit configuration_r() noexcept = default;
-
+        constexpr explicit configuration_r() noexcept = default;
     };
 };
 
--- src/serial/i2cdevices/lm75.cpp
+++ src/serial/i2cdevices/lm75.cpp
@@ -3,12 +3,10 @@
 namespace muonpi::serial::devices {
 
 lm75::lm75(traffic_t& bus_traffic, const std::string& path, i2c_device::address_type address)
-    : i2c_device { bus_traffic, path, address} {}
+    : i2c_device {bus_traffic, path, address} {}
 
 lm75::lm75(traffic_t& bus_traffic, const std::string& path)
-    : i2c_device { bus_traffic, path, addresses} {}
-
-
+    : i2c_device {bus_traffic, path, addresses} {}
 
 auto lm75::identify() -> bool {
     if (flag_set(Flags::Error)) {
@@ -18,28 +16,28 @@ auto lm75::identify() -> bool {
         return false;
     }
 
-    const auto config { read<configuration_r>() };
+    const auto config {read<configuration_r>()};
     if (!config.has_value()) {
         return false;
     }
     if (config.value().reserved != 0) {
         return false;
     }
-    const auto temp { read<temperature_r>() };
+    const auto temp {read<temperature_r>()};
     if (!temp.has_value()) {
         return false;
     }
     if (temp.value().reserved != 0) {
         return false;
     }
-    const auto thyst { read<t_hyst_r>() };
+    const auto thyst {read<t_hyst_r>()};
     if (!thyst.has_value()) {
         return false;
     }
     if (thyst.value().reserved != 0) {
         return false;
     }
-    const auto tos { read<t_os_r>() };
+    const auto tos {read<t_os_r>()};
     if (!tos.has_value()) {
         return false;
     }
